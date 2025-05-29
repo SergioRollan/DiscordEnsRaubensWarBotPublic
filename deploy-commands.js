0354e860a0930a84410f6cb86e9cf052329d1c7e
Sergio Rollán <sjrolmor@gmail.com>
2025-05-29 22:50:53 +0200

    a

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,25 +2,8 @@ require("dotenv").config();
 
 const { REST, Routes } = require("discord.js");
 const fs = require("fs");
-const path = require("path");
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
-
-(async () => {
-  try {
-    // Borra comandos de guild
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-      body: [],
-    });
-    // Borra comandos globales (opcional, si alguna vez los usaste)
-    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
-      body: [],
-    });
-    console.log("Comandos eliminados.");
-  } catch (error) {
-    console.error(error);
-  }
-})();
 
 const commands = [];
 const commandFiles = fs.readdirSync("./commands");
